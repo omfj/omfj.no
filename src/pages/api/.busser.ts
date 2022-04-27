640b651022434ecd4a5b89623982fc55c2ce58c6
Ole Magnus Johnsen <ole.magnus@me.com>
2022-04-27 03:51:31 +0200

    remove api

--- src/pages/api/.busser.ts
+++
@@ -1,8 +0,0 @@
-const busses = require("./russebusser.json")
-
-// TODO Fix types
-const Handler = ({res, req}: {res: any, req: any}) => {
-  res.status(200).json(busses)
-}
-
-export default Handler;

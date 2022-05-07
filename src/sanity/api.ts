453575f3a9126ae23fbb96d7f6f2172e452b5f03
Ole Magnus Johnsen <ole.magnus@me.com>
2022-05-07 03:17:27 +0200

    Revert to ":art: Production as default dataset"

--- src/sanity/api.ts
+++ src/sanity/api.ts
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client';
 
 const SanityAPI = sanityClient({
     projectId: '28zlbntv',
-    dataset: process.env.SANITY_DATASET ?? 'production',
+    dataset: 'production',
     apiVersion: '2021-10-21',
     token: process.env.SANITY_TOKEN,
     useCdn: true,

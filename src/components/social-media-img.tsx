1b6104a4a7ae3d16eff468688925790424539794
Ole Magnus Johnsen <ole.magnus@me.com>
2022-05-15 11:07:46 +0200

    🐛 Added anchor tag to social media images

--- src/components/social-media-img.tsx
+++ src/components/social-media-img.tsx
@@ -1,6 +1,6 @@
 import NextLink from "next/link";
 import Image from "next/image";
-import { Box, Link } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 interface Props {
   source: any;
@@ -11,9 +11,11 @@ interface Props {
 const SocialMediaImg = ({ source, href, alt }: Props): JSX.Element => {
   return (
     <NextLink href={href} passHref>
-      <Box w="125px" h="125px" _hover={{ cursor: "pointer" }}>
-        <Image src={source} alt={alt} />
-      </Box>
+      <a>
+        <Box w="125px" h="125px" _hover={{ cursor: "pointer" }}>
+          <Image src={source} alt={alt} />
+        </Box>
+      </a>
     </NextLink>
   );
 };

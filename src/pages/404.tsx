a1d39d49cdaab47727e1269f6020b7b6a13888cb
Ole Magnus Johnsen <ole.magnus@me.com>
2022-05-15 11:14:21 +0200

    💄 Better 404 page design

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,7 @@
-import { Center, Button, Heading, Text } from "@chakra-ui/react";
+import { Center, Box, Heading, Text } from "@chakra-ui/react";
 import NextLink from "next/link";
+import Image from "next/image";
+import * as Emoji from "../../public/images/thinkingemoji.png";
 import Main from "../components/main";
 import SEO from "../components/SEO";
 
@@ -10,12 +12,19 @@ const Error = (): JSX.Element => {
       <Main>
         <Center flexDir="column" textAlign="center">
           <Heading>The page does not exist.</Heading>
+          <NextLink href="/" passHref>
+            <a>
+              <Box pos="relative" w="200px" h="200px" overflow="hidden">
+                <Image
+                  src={Emoji}
+                  alt="Emoji thinking"
+                  objectFit="cover"
+                  layout="fill"
+                />
+              </Box>
+            </a>
+          </NextLink>
           <Text fontSize="3xl">Error 404</Text>
-          <Button my="5" size="lg">
-            <NextLink href="/" passHref>
-              Home
-            </NextLink>
-          </Button>
         </Center>
       </Main>
     </>

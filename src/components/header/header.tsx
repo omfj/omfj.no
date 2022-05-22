113980991c729b846875d0ed882e5334df550742
Ole Magnus Johnsen <ole.magnus@me.com>
2022-05-23 01:36:05 +0200

    🧱 Body no longer scrolls when drawer is open

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,27 +3,35 @@ import { AiFillPhone, AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { NavLinkDesktop, NavLinkMobile } from "./navlinks";
 import { BiGitBranch } from "react-icons/bi";
 import { FiPackage } from "react-icons/fi";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
-const Header = (): JSX.Element => {
-  const [open, isOpen] = useState(true);
+const Header = () => {
+  const [open, isOpen] = useState(false);
 
   const variants = {
     open: { opacity: 1, x: 0 },
     closed: { opacity: 0, x: "-100%", display: "none" },
   };
 
+  useEffect(() => {
+    if (open) {
+      document.body.style.position = "fixed";
+    } else {
+      document.body.style.position = "static";
+    }
+  }, [open]);
+
   return (
     <div>
-      <nav className="md:px-10 md:flex md:justify-between">
+      <nav className="px-4 md:px-10 md:flex md:justify-between transition-transform">
         <div className="flex justify-between items-center p-4">
           <HeaderLogo />
           <div className="md:hidden pt-2">
-            {open && (
+            {!open && (
               <AiOutlineMenu
                 className="hover:cursor-pointer"
-                size={30}
+                size={40}
                 onClick={() => isOpen(!open)}
               />
             )}
@@ -48,17 +56,17 @@ const Header = (): JSX.Element => {
         </div>
         <div
           className={
-            !open
-              ? `fixed z-50 overflow-hidden top-0 right-0 bottom-0 left-0 h-full w-full object-fill p-10 bg-[#070707] md:hidden`
+            open
+              ? `fixed block z-50 top-0 right-0 bottom-0 left-0 h-full w-full p-14 bg-[#070707] md:hidden`
               : "hidden"
           }
         >
-          <AiOutlineClose
-            className="hover:cursor-pointer"
-            size={40}
-            onClick={() => isOpen(!open)}
-          />
-          <motion.div animate={!open ? "open" : "closed"} variants={variants}>
+          <motion.div animate={open ? "open" : "closed"} variants={variants}>
+            <AiOutlineClose
+              className="hover:cursor-pointer"
+              size={40}
+              onClick={() => isOpen(!open)}
+            />
             <div className="mt-5" onClick={() => isOpen(!open)}>
               <NavLinkMobile text="home" to="/" />
               <NavLinkMobile text="projects" to="/projects/" />

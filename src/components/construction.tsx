6d6144660c78b56b13887ef1e50726827420ef52
Ole Magnus Johnsen <ole.magnus@me.com>
2022-05-24 15:41:11 +0200

    🐛 Construction class not working on light mode

--- src/components/construction.tsx
+++ src/components/construction.tsx
@@ -5,10 +5,10 @@ interface Props {
 const Construction = ({ title }: Props): JSX.Element => {
   return (
     <div className="construction">
-      <div className="flex flex-col bg-[#070707] p-10 rounded border-2 border-[#070707]">
-        <p className="text-3xl text-bold">{title}</p>
+      <div className="flex flex-col bg-slate-50 dark:bg-[#070707] p-10 rounded border-2 border-[#070707]">
+        <p className="text-3xl font-bold">{title}</p>
         <p className="text-xl">
-          Hey! You have entered a construction site! Come back later.
+          🚧 Hey! You have entered a construction site! Come back later. 🚧
         </p>
       </div>
     </div>

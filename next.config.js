7231471a20b189048f344a130ba11c532371ea29
Ole Magnus Fon Johnsen <ole.magnus@me.com>
2022-08-29 04:46:47 +0200

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable: process.env.NODE_ENV === "development",
+});
+
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = {
+module.exports = withPWA({
   nextConfig,
   trailingSlash: true,
   images: {
     domains: ["cdn.sanity.io"],
   },
-};
+});
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,27 @@ const Document = () => {
           content="omfj, ole magnus, ole magnus fon johnsen, olem"
         />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       </Head>
       <body>
         <Main />

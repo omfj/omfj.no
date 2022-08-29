f459a4605e50335e5c395a99d3dfd2e800c3ffc1
Ole Magnus Fon Johnsen <ole.magnus@me.com>
2022-08-29 04:06:55 +0200

    fix build error

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,10 +1,16 @@
+import { CSSProperties } from "react";
+
 interface Props {
   children: React.ReactNode;
   className?: string;
+  style?: CSSProperties;
 }
 
-const Heading = ({ children, className }: Props) => (
-  <h1 className={`font-bold text-center text-3xl md:text-4xl p-2 ${className}`}>
+const Heading = ({ children, className, style }: Props) => (
+  <h1
+    className={`font-bold text-center text-3xl md:text-4xl p-2 ${className}`}
+    style={style ?? {}}
+  >
     {children}
   </h1>
 );

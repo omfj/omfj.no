1af33bee6da4908acf3821aa61266744df39f972
Ole Magnus Fon Johnsen <me@omfj.no>
2023-06-01 16:09:55 +0200

    Fix fonts(?)

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -18,12 +18,13 @@ export const metadata: Metadata = {
   },
 };
 
-export const inter = Inter({
+const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
+  weight: ["400", "700"],
 });
 
-export const ibmPlexMono = IBM_Plex_Mono({
+const ibmPlexMono = IBM_Plex_Mono({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-ibm-plex-mono",

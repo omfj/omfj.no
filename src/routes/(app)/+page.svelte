fea39fb1e276e2c2ee946ae47b2a14d3c571e28d
Ole Magnus Fon Johnsen <me@omfj.no>
2025-01-04 22:08:51 +0100

    Change title

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -6,7 +6,7 @@
 
 <div class="py-12 md:py-24 transition-all">
 	<header class="max-w-xl mx-auto p-8">
-		<h1 class="text-3xl">omfj</h1>
+		<h1 class="text-3xl">omfj.no</h1>
 	</header>
 
 	<main class="max-w-xl mx-auto p-8 mb-10">

c54a0e3922f007f7218486f8b49163b8581bcf3d
Ole Magnus Fon Johnsen <me@omfj.no>
2025-07-14 21:19:54 +0200

    Update cloudflare config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
 });

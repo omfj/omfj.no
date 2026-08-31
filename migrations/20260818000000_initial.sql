3db4a1227a1435322662a958808aeb2780781a91
Ole Magnus Fon Johnsen <me@omfj.no>
2026-08-31 22:52:13 +0200

    chore: fix sql checksum

--- migrations/20260818000000_initial.sql
+++ migrations/20260818000000_initial.sql
@@ -10,14 +10,17 @@ CREATE TABLE films (
 CREATE TABLE links (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
-    url TEXT NOT NULL
+    url TEXT NOT NULL,
+    hostname TEXT NOT NULL,
+    sort_order INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE wishes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     url TEXT,
-    notes TEXT
+    notes TEXT,
+    sort_order INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE sessions (
